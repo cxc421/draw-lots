ae95a6c0830c90674587cf3ccb9ff58a3cf11e21
Chris Chuang <chrischuang0421@gmail.com>
2019-04-29 16:32:21 +0800

    Updates

---
+++ precache-manifest.a0c4b7e188c1976cfd876fb4062d07df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b320c4730ae55b2fa35302d1df3d4ae",
+    "url": "/draw-lots/index.html"
+  },
+  {
+    "revision": "1d6d710441f24e1e2456",
+    "url": "/draw-lots/static/css/main.adf8629d.chunk.css"
+  },
+  {
+    "revision": "4e27d9f03d70a9ab2210",
+    "url": "/draw-lots/static/js/2.9edf1ad3.chunk.js"
+  },
+  {
+    "revision": "1d6d710441f24e1e2456",
+    "url": "/draw-lots/static/js/main.ab323bf6.chunk.js"
+  },
+  {
+    "revision": "00866f6892ac63a590a8",
+    "url": "/draw-lots/static/js/runtime~main.a7b18b6d.js"
+  },
+  {
+    "revision": "e7f84adc0819275b9d39f9deb63dafa7",
+    "url": "/draw-lots/static/media/bell.e7f84adc.mp3"
+  },
+  {
+    "revision": "f1840088d971b09cf10a62d349ceb99f",
+    "url": "/draw-lots/static/media/bg.f1840088.jpg"
+  },
+  {
+    "revision": "da8299d34d9e922d54e9f01a2631ba75",
+    "url": "/draw-lots/static/media/bg_small.da8299d3.jpg"
+  }
+]);
